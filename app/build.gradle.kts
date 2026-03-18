d458f1205fd781475a4dea3ec95efbb819ec9f67
Matvey <romankovmatvej59@gmail.com>
2026-03-19 05:05:59 +0700

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.12.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.12.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 }

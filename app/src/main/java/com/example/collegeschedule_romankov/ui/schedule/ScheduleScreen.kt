ba16afdb7c74be07a701ac0a61dd51c806bf02a9
Matvey <romankovmatvej59@gmail.com>
2026-03-19 09:04:33 +0700

    Update ScheduleScreen

--- app/src/main/java/com/example/collegeschedule_romankov/ui/schedule/ScheduleScreen.kt
+++ app/src/main/java/com/example/collegeschedule_romankov/ui/schedule/ScheduleScreen.kt
@@ -1,5 +1,6 @@
 package com.example.collegeschedule_romankov.ui.schedule
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.example.collegeschedule_romankov.data.dto.ScheduleByDateDto
 import com.example.collegeschedule_romankov.data.network.RetrofitInstance
+import com.example.collegeschedule_romankov.ui.components.GroupDropdown
 import com.example.collegeschedule_romankov.utils.getWeekDateRange
 
 @Composable
@@ -17,17 +19,30 @@ fun ScheduleScreen() {
     var schedule by remember {
         mutableStateOf<List<ScheduleByDateDto>>(emptyList())
     }
+    var groups by remember {
+        mutableStateOf<List<String>>(emptyList())
+    }
+    var selectedGroup by remember { mutableStateOf("") }
     var loading by remember { mutableStateOf(true) }
     var error by remember { mutableStateOf<String?>(null) }
 
-    LaunchedEffect(Unit) {
+    LaunchedEffect(selectedGroup) {
         val (start, end) = getWeekDateRange()
         try {
-            schedule = RetrofitInstance.api.getSchedule(
-                "ИС-12",
-                start,
-                end
-            )
+            if (groups.isEmpty()) {
+                groups = RetrofitInstance.api.getGroups().map { it.groupName }
+            }
+
+            if (selectedGroup.isNotBlank()) {
+                schedule = RetrofitInstance.api.getSchedule(
+                    selectedGroup,
+                    start,
+                    end
+                )
+            } else {
+                schedule = emptyList()
+            }
+
         } catch (e: Exception) {
             error = e.message
         } finally {
@@ -35,9 +50,19 @@ fun ScheduleScreen() {
         }
     }
 
-    when {
-        loading -> CircularProgressIndicator()
-        error != null -> Text("Ошибка: $error")
-        else -> ScheduleList(schedule)
+    Column {
+        GroupDropdown(
+            groups = groups,
+            selectedGroup = selectedGroup,
+            onGroupSelected = { group ->
+                selectedGroup = group
+            }
+        )
+
+        when {
+            loading -> CircularProgressIndicator()
+            error != null -> Text("Ошибка: $error")
+            else -> ScheduleList(schedule)
+        }
     }
 }
